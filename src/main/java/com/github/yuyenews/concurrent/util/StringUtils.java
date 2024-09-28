8ae963ae023746f3b81a5a856a1db8290188bdba
Beerus <yuyemail@163.com>
2024-09-28 18:07:46 +0800

    首次提交

--- src/main/java/com/github/yuyenews/concurrent/util/StringUtils.java
+++ src/main/java/com/github/yuyenews/concurrent/util/StringUtils.java
@@ -1,7 +1,15 @@
 package com.github.yuyenews.concurrent.util;
 
+/**
+ * 字符串工具类
+ */
 public class StringUtils {
 
+    /**
+     * 判断是否为空
+     * @param s
+     * @return
+     */
     public static boolean isEmpty(String s){
         if(s == null || s.trim().equals("")){
             return true;
@@ -9,6 +17,11 @@ public class StringUtils {
         return false;
     }
 
+    /**
+     * 判断是否不为空
+     * @param s
+     * @return
+     */
     public static boolean isNotEmpty(String s){
         return isEmpty(s) == false;
     }
